22169b095bafdf50c7b89aa70daadf0dd2eb4d63
hoomzy <hoomzy@DESKTOP-BSPB1C9>
2022-08-04 18:42:04 +0900

    효주수정333

--- src/main/java/com/spring/boardweb/controller/board/BoardController.java
+++ src/main/java/com/spring/boardweb/controller/board/BoardController.java
@@ -35,7 +35,7 @@ public class BoardController {
 	
 	@Autowired
 	BoardService boardService;
-	//효주수정222
+	//효주수정222333
 	@GetMapping("/getNotice")
 	public ModelAndView getNoticeView(Notice notice,
 									  @PageableDefault(page = 0, size = 10) Pageable pageable) {
